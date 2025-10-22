2736a673926ad6b13ddec1c09b5a3a5ae826b8f4
khs29216 <khs29216@gmail.com>
2025-10-23 06:41:04 +0900

    refactor: InputTest 메서드 이름 수정
    - 메서드 이름을 수정함으로써 가독성을 높임

--- src/test/java/racingcar/InputTest.java
+++ src/test/java/racingcar/InputTest.java
@@ -11,20 +11,20 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 public class InputTest {
     @Test
     @DisplayName("자동차 이름을 정상적으로 입력했을 경우, 자동차 이름 리스트를 반환한다.")
-    void 자동차이름_정상적인_입력_테스트() {
+    void parseCarNames_정상입력() {
         // given
-        String carNamesInput = "pobi,woni,jun";
+        String expectedCarNamesInput = "pobi,woni,jun";
 
         // when
-        List<String> carNameList = InputManager.parseCarNames(carNamesInput);
+        List<String> actualCarNameList = InputManager.parseCarNames(expectedCarNamesInput);
 
         // then
-        assertThat(carNameList).containsExactly("pobi","woni","jun");
+        assertThat(actualCarNameList).containsExactly("pobi", "woni", "jun");
     }
 
     @Test
     @DisplayName("자동차 이름 리스트로 Car 객체 리스트를 생성한다")
-    void Car_리스트_생성_테스트() {
+    void createCarListFromNames_정상입력() {
         // given
         List<String> expectedCarNameList = List.of("pobi", "woni", "jun");
 
