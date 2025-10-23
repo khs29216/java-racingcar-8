1f4af892d27ef8391d77a1b98d78bf3e5e6d6308
khs29216 <khs29216@gmail.com>
2025-10-24 06:59:32 +0900

    feat: `input()` 메서드 추가
    콘솔에서 사용자 입력을 받기 위해 `input()` 메서드 구현.

--- src/main/java/racingcar/InputManager.java
+++ src/main/java/racingcar/InputManager.java
@@ -1,9 +1,15 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class InputManager {
+
+    public static String input() {
+        return Console.readLine();
+    }
 
     public static List<String> parseCarNames(String carNamesInput) {
         return Arrays.stream(carNamesInput.split(",")).toList();
