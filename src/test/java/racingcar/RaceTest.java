ebaf71d40b006ed5e2efa8c40872501824a8ddce
khs29216 <khs29216@gmail.com>
2025-10-24 09:23:09 +0900

    test: 시도 횟수만큼 경주 진행 테스트 추가

--- src/test/java/racingcar/RaceTest.java
+++ src/test/java/racingcar/RaceTest.java
@@ -27,4 +27,24 @@ public class RaceTest {
 
         assertThat(actualPositions).isEqualTo(expectedPositions);
     }
+
+    @Test
+    @DisplayName("시도 횟수만큼 경주를 진행한다.")
+    void startRace_테스트() {
+        // given
+        List<Car> carList = List.of(new Car("pobi"), new Car("woni"), new Car("jun"));
+        Race race = new Race(3);
+        List<List<Integer>> roundRandomNumbers = List.of(List.of(5, 3, 7), List.of(1, 1, 1), List.of(6, 3, 3));
+        List<Integer> expectedPositions = List.of(2, 0, 1);
+
+        // when
+        race.startRace(carList, roundRandomNumbers);
+
+        // then
+        List<Integer> actualPositions = carList.stream()
+                .map(Car::getPosition)
+                .toList();
+
+        assertThat(actualPositions).isEqualTo(expectedPositions);
+    }
 }
