26f5c6252c6d5e82b80a086e1d1f8494998493f3
khs29216 <khs29216@gmail.com>
2025-10-24 09:00:32 +0900

    feat: 라운드 진행 테스트 통과용 최소 구현
    TDD Green 단계로 라운드 진행 테스트 통과를 위해 최소 구현을 진행했다.
    
    Race 클래스에 테스트용으로 랜덤값을 외부에서 주입하는
    playRound(..., List<Integer> randomNumbers) 메서드를 구현했다.
    추후, 실제로 사용할 playRound(...) 메서드는 랜덤값을 내부에서 생성하도록 오버로딩할 예정이다.

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,10 +1,20 @@
 package racingcar;
 
+import java.util.List;
+
 public class Race {
     private final int roundCount;
 
     public Race(int roundCount) {
         this.roundCount = roundCount;
+    }
+
+    // 테스트용 playRound 메서드
+    public static void playRound(List<Car> carList, List<Integer> randomNumbers) {
+        for (int i = 0; i < carList.size(); i++) {
+            Car car = carList.get(i);
+            car.move(randomNumbers.get(i));
+        }
     }
 
     public int getRoundCount() {
