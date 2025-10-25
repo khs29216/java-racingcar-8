c0f73f2cf264d292043d48d770f4ed1de9c83fef
khs29216 <khs29216@gmail.com>
2025-10-25 17:30:51 +0900

    refactor: 자동차 위치 출력 로직 메서드 분리
    유지보수성과 가독성 향상을 위해 메서드를 분리했다.

--- src/main/java/racingcar/OutputManager.java
+++ src/main/java/racingcar/OutputManager.java
@@ -7,10 +7,14 @@ public class OutputManager {
     public static void roundOutput(List<Car> carList) {
         for (Car car : carList) {
             System.out.print(car.getName() + " : ");
-            for (int i = 0; i < car.getPosition(); i++) {
-                System.out.print("-");
-            }
+            printPosition(car);
             System.out.println();
+        }
+    }
+
+    private static void printPosition(Car car) {
+        for (int i = 0; i < car.getPosition(); i++) {
+            System.out.print("-");
         }
     }
 }
