90e0bbd9f53cc915296c4913498697315fda6eb5
khs29216 <khs29216@gmail.com>
2025-10-26 16:27:14 +0900

    feat: 프로그램 실행을 위한 Controller 구성 및 Application 연동
    Controller에서 사용자 입력부터 경주 진행까지 프로그램 흐름을 관리하도록
    execute 메서드를 구현했다.
    Application에서 Controller를 실행하여 프로그램이 전체적으로 동작하도록 연결하였다.

---
+++ src/main/java/racingcar/Controller.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+import java.util.List;
+
+public class Controller {
+
+    public void execute() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNamesInput = InputManager.input();
+        List<String> carNameList = CarNameParser.parseCarNames(carNamesInput);
+        List<Car> carList = CarFactory.createCarListFromNames(carNameList);
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String roundCount = InputManager.input();
+        Race race = new Race(Integer.parseInt(roundCount));
+
+        race.startRace(carList);
+    }
+}
